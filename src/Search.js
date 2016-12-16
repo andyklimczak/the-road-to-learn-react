e019490c4891b524adb1da95424d755756213a98
Andy Klimczak <andyklimczak@fastmail.com>
2016-12-16 14:56:36 -0500

    different component declarations

--- src/Search.js
+++ src/Search.js
@@ -2,15 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children}<input type="text" value={value} onChange={onChange} />
-      </form>
-    );
-  }
-}
+const Search = ({value, onChange, children }) =>
+  <form>
+    {children} <input type="text" value={value} onChange={onChange} />
+  </form>
 
 export default Search;
--- src/Table.js
+++ src/Table.js
@@ -4,24 +4,18 @@ import './App.css';
 
 const isSearched = (query) => (item) => !query || item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1;
 
-class Table extends Component {
-  render() {
-    const { list, pattern } = this.props;
-    return (
-      <div>
-        { list.filter(isSearched(pattern)).map(item => {
-          return (
-            <div key={item.objectId}>
-              <span><a href={item.url}>{item.title}</a></span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+const Table = ({list, pattern}) =>
+  <div>
+    { list.filter(isSearched(pattern)).map(item => {
+      return (
+        <div key={item.objectId}>
+          <span><a href={item.url}>{item.title}</a></span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+      );
+    })}
+  </div>
 
 export default Table;
