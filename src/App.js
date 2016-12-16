f202b7e49d2b58a88527be5a283eefce7e96a442
Andy Klimczak <andyklimczak@fastmail.com>
2016-12-16 14:09:50 -0500

    interactions in components

--- src/App.js
+++ src/App.js
@@ -19,20 +19,30 @@ const list = [
     objectId: 1,
   },
 ];
+const isSearched = (query) => (item) => !query || item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       list,
+      query: '',
     };
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+  onSearchChange(event) {
+    this.setState({query: event.target.value});
   }
   render() {
+    const { query, list } = this.state;
     return (
       <div className="App">
-        { this.state.list.map((item, key) => {
+        <form>
+          <input type="text" value={query} onChange={this.onSearchChange} />
+        </form>
+        { list.filter(isSearched(query)).map(item => {
           return (
-            <div key={key}>
+            <div key={item.objectId}>
               <span><a href={item.url}>{item.title}</a></span>
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
